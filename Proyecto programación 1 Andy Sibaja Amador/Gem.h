427f8f6f94a966f4900c83f58ae5d2331b86ffdb
AndySibaja <222462087+AndySAUNA@users.noreply.github.com>
2025-09-13 18:21:45 -0600

    ver 0.1
    Clase gemas funciona y dibuja correctamente sobre la tabla

--- Proyecto programación 1 Andy Sibaja Amador/Gem.h
+++ Proyecto programación 1 Andy Sibaja Amador/Gem.h
@@ -1,19 +1,61 @@
 #pragma once
+#include <SFML/Graphics.hpp>
+#include <SFML/Window.hpp>
+#include <SFML/System.hpp>
+using namespace sf;
 #include <string>
 #include <iostream>
+#include <random>
 using namespace std;
+
 
 class Gem {
 private:
 	int positionx, positiony;
 	int gemtype;
+
 public:
 	Gem(int positionx, int positiony) {
+		srand((unsigned)time(0));
 		this->positionx = positionx;
 		this->positiony = positiony;
-		gemtype = rand() % 5;
+		gemtype = (rand() % 5)+1;
 	}
-	int setpositionx(int positionx) { this->positionx = positionx; }
-	int setpositiony(int positiony) { this->positiony = positiony; }
+	void setpositionx(int positionx) { this->positionx = positionx; }
+	void setpositiony(int positiony) { this->positiony = positiony; }
+	int getgemtype() { return gemtype; }
+	// this function draws a gem based on its type and position;
+	void draw(RenderWindow& window,int i, int j) {
+		if (gemtype == 1) {
+			CircleShape shape(30.f);
+			shape.setFillColor(Color::Red);
+			shape.setPosition(Vector2f((i * 75.f + 205.f), (j * 75.f + 5)));
+			window.draw(shape);
+		}
+		if (gemtype == 2) {
+			CircleShape shape(30.f,3);
+			shape.setFillColor(Color::Green);
+			shape.setPosition(Vector2f((i * 75.f + 205.f), (j * 75.f + 5)));
+			window.draw(shape);
+		}
+		if (gemtype == 3) {
+			CircleShape shape(30.f, 4);
+			shape.setFillColor(Color::Blue);
+			shape.setPosition(Vector2f((i * 75.f + 205.f), (j * 75.f + 5)));
+			window.draw(shape);
+		}
+		if (gemtype == 4) {
+			CircleShape shape(30.f, 5);
+			shape.setFillColor(Color::Yellow);
+			shape.setPosition(Vector2f((i * 75.f + 205.f), (j * 75.f + 5)));
+			window.draw(shape);
+		}
+		if (gemtype == 5) {
+			CircleShape shape(30.f, 6);
+			shape.setFillColor(Color::Cyan);
+			shape.setPosition(Vector2f((i * 75.f + 205.f), (j * 75.f + 5)));
+			window.draw(shape);
+		}
+	}
 
 };
--- Proyecto programación 1 Andy Sibaja Amador/Proyecto programación 1 Andy Sibaja Amador.cpp
+++ Proyecto programación 1 Andy Sibaja Amador/Proyecto programación 1 Andy Sibaja Amador.cpp
@@ -1,22 +1,50 @@
 #include <SFML/Graphics.hpp>
+#include <SFML/Window.hpp>
+#include <SFML/System.hpp>
+using namespace sf;
+using namespace std;
+#include <iostream>
+#include "Gem.h"
 
 int main()
 {
-    sf::RenderWindow window(sf::VideoMode(200, 200), "SFML works!");
-    sf::CircleShape shape(100.f);
-    shape.setFillColor(sf::Color::Green);
+    RenderWindow window(VideoMode(800, 600), "Proyecto programación 1 Andy Sibaja");
+    window.setFramerateLimit(60);
+    RectangleShape rect(Vector2f(75.f, 75.f));
+    Color test(127, 255, 212);
+    rect.setFillColor(test);
+    rect.setOutlineColor(Color::Black);
+	rect.setOutlineThickness(5.f);
+
+    Gem gem1(0,0);
 
     while (window.isOpen())
     {
-        sf::Event event;
+        Event event;
         while (window.pollEvent(event))
         {
-            if (event.type == sf::Event::Closed)
+            if (event.type == Event::Closed)
                 window.close();
+        }
+        if (event.type == Event::MouseButtonPressed)
+        {
+            if (event.mouseButton.button == sf::Mouse::Left)
+            {
+                std::cout << "the left button was pressed" << std::endl;
+                std::cout << "mouse x: " << event.mouseButton.x << std::endl;
+                std::cout << "mouse y: " << event.mouseButton.y << std::endl;
+            }
         }
 
         window.clear();
-        window.draw(shape);
+        //ment to draw a 8x8 grid of squares on the right side of the window
+        for (float i = 0; i < 8; i++) {
+            for (float j = 0; j < 8; j++) {
+                rect.setPosition(Vector2f((i*75.f + 200.f), (j*75.f)));
+                window.draw(rect);
+            }
+        }
+        gem1.draw(window, 0, 0);
         window.display();
     }
 
